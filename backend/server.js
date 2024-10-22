c9ad7573813246f63dce8136a88cd98095887c1d
IK <khushijukanti33@gmail.com>
2024-10-23 02:56:16 +0530

    update sever file for managing products, catrs, orders, payments and user routes

--- backend/server.js
+++ backend/server.js
@@ -9,14 +9,27 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // MongoDB connection
-mongoose.connect(process.env.Mongo_Uri, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.Mongo_Uri)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
 // Routes
-app.use('/api/users', require('./routes/userRoutes'));
-app.use('/api/products', require('./routes/productRoutes'));
-app.use('/api/payments', require('./routes/paymentRoutes'));
+app.use('/users', require('./routes/user'));
+app.use('/products', require('./routes/product'));
+app.use('/payments', require('./routes/payment'));
+app.use('/cart', require('./routes/cart'));
+app.use('/orders', require('./routes/order'))
+app.use('/uploads', express.static('uploads'));
 
 const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+
+
+
+
+
+// const crypto = require('crypto');
+// const jwtSecret = crypto.randomBytes(64).toString('hex');
+// console.log(jwtSecret);
