847faee6e11e2a0c80b565c98623f9d264c659e6
IK <khushijukanti33@gmail.com>
2024-10-23 03:02:06 +0530

    add browser routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,34 @@
-import './App.css';
-function App() {
-  return (
-    <div className="App">
-      Hi
-    </div>
-  );
-}
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import { AuthProvider } from './context/AuthContext'; // Import AuthProvider
+import Register from './components/Register';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+import EcommerceNavbar from './components/Navbar';
+import ProductComponent from './components/ProductManagement';
+import Cart from './components/Cart';
+import Payments from './components/Checkout';
+import Orders from './components/Orders';
+
+const App = () => {
+    return (
+        <AuthProvider>
+            <Router>
+                <EcommerceNavbar />
+                <Routes>
+                    <Route path="/" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/admin" element={<ProductComponent />} />
+                    <Route path="/cart" element={<Cart />} />
+                    <Route path="/orders" element={<Orders/>} />
+                    <Route path="/checkout" element={<Payments />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    {/* Add other routes as needed */}
+                </Routes>
+            </Router>
+        </AuthProvider>
+    );
+};
 
 export default App;
